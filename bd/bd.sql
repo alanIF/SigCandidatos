c6be723588b507cbe182bb4d2778afbc6e766a92
Alan Tavares <alan_tj@hotmail.com.br>
2020-03-01 09:47:38 -0300

    banco de dados

---
+++ bd/bd.sql
@@ -0,0 +1,49 @@
+create database sig_candidatos;
+use sig_candidatos;
+
+create table usuario( 
+	id int auto_increment not null,
+	nome varchar(200) not null,
+	email varchar(200) not null,
+	senha varchar(200) not null,
+	primary key(id)
+);
+
+create table candidato(
+	id int auto_increment not null,
+	nome text not null,
+	descricao text, 
+	partido text,
+	data_nascimento text,
+	rede_social text, 
+	primary key(id)
+); 
+
+
+create table proposta(
+	id int auto_increment not null,
+	id_candidato int not null,
+	titulo text not null,
+	descricao int not null,
+	primary key(id),
+	foreign key(id_candidato) references candidato(id)
+);
+
+create table conquista(
+	id int auto_increment not null,
+	id_candidato int not null,
+	titulo text not null,
+	descricao int not null,
+	primary key(id),
+	foreign key(id_candidato) references candidato(id)
+);
+
+create table denuncia(
+	id int auto_increment not null,
+	nome_reclamante text not null,
+	nome_denuciado text not null, 
+	descricao text, 
+	data_denuncia text,
+	primary key(id)
+
+);
